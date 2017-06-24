53170dbdcc87190fe6a1c817004c083e5c37d670
Kare Koho <kare.koho@yahoo.com>
2017-06-24 09:34:20 +0300

    Update examples

--- src/examples/revive.h
+++ src/examples/revive.h
@@ -13,6 +13,10 @@ fn_reviver (const wchar_t *key, value *val)
   if (wcscmp (key, L"Thumbnail") == 0) // Remove the Thumbnail object
     return new undefined ();
 
+  if (wcscmp (key, L"Description") == 0
+      && val->type () == value::null_t) // Change the value
+      return new string (L"Not available"); // FIXME: /any_letter e.g. n/a, causes segmantation fault in format::string
+
   return val;
 }
 
@@ -26,6 +30,7 @@ revive ()
           \"Width\":  800,\
           \"Height\": 600,\
           \"Title\":  \"View from 15th Floor\",\
+          \"Description\":null,\
           \"Thumbnail\": {\
               \"Url\":    \"http://www.example.com/image/481989943\",\
               \"Height\": 125,\
@@ -37,8 +42,8 @@ revive ()
     }", fn_reviver);
 
   std::wcout << v->stringify () << std::endl;
-  // output: {"Image":{"Animated":true,"Title":"View from 15th Floor",
-  // "Height":600,"IDs":[116,943,234,38793],"Width":800}}
+  // output: {"Image":{"IDs":[116,943,234,38793],"Description":"Not available",
+  // "Height":600,"Animated":true,"Title":"View from 15th Floor","Width":800}}
 }
 
 #endif // REVIVE_H
