bf3c723285e01f7402b846437b3e2766cb325ce7
Kare Koho <kare.koho@yahoo.com>
2023-01-30 19:27:05 +0200

    Remove commened cppunit tests

--- src/test/json_array_iterator_test.h
+++ src/test/json_array_iterator_test.h
@@ -2,6 +2,7 @@
 #define JSON_ARRAY_ITERATOR_TEST
 
 #include "unit_test.h"
+#include "json_mock_value.h"
 
 namespace format
 {
@@ -159,150 +160,7 @@ namespace format
         // test::array_iterator_test::test_debug ()
       }
     }
-  /**
-   * 10.
-   *
-   * @brief The json_array_iterator_test class
-   */
-  class json_array_iterator_test : public unit_test
-  {
-  public:
-    void
-    test_construct_assign_destruct ()
-    {
-//      array::element_list list = { new boolean };
-
-//      array::const_iterator *it[] = {
-//        new array::const_iterator (),
-//        new array::const_iterator (list.begin ()),
-//      };
-
-//      array::const_iterator copy = array::const_iterator (*it[1]);
-
-//      CPPUNIT_ASSERT_MESSAGE ("& copy != it[1]", & copy != it[1]);
-
-//      delete it[0];
-//      delete it[1];
-
-//      (void) element_list_clear (list);
-    }
-
-    void
-    test_dereference ()
-    {
-//      array a = { new boolean };
-
-//      array::const_iterator it = a.begin ();
-//      array::const_iterator copy = array::const_iterator (it);
-
-//      struct assert {
-//        array::const_iterator *itp;
-//        value::value_t type;
-//        int assert_status;
-//      };
-
-//      std::vector<struct assert> test = {
-//        { & it, value::boolean_t, PASS_T },
-//        { & copy, value::boolean_t, PASS_T }
-//      };
-
-//      TEST_IT_START
-
-//          value & v = **(*it).itp;
-
-//          ASSERT_EQUAL_IDX ("v.type ()", value::value_t::boolean_t, v.type ());
-
-//      TEST_IT_END;
-    }
-
-    void
-    test_pre_increment ()
-    {
-//      array a = { new boolean, new boolean };
-
-//      array::const_iterator begin = a.begin ();
-//      array::const_iterator end = a.end ();
-
-//      size_t count = 0;
-
-//      for (auto it = begin; it != end; ++it)
-//        count++;
-
-//      CPPUNIT_ASSERT_EQUAL_MESSAGE ("count", (size_t) 2, count);
-    }
-
-    void
-    test_post_increment ()
-    {
-//      array a = { new boolean };
-//      array::const_iterator begin = a.begin ();
-//      array::const_iterator current = begin;
-
-//      CPPUNIT_ASSERT_MESSAGE ("current++ == begin", current++ == begin);
-//      CPPUNIT_ASSERT_MESSAGE ("current != begin", current != begin);
-    }
-
-    void
-    test_begin_end ()
-    {
-//      array_accessor a = L"[0,1]";
-
-//      array::const_iterator begin (a.begin ());
-//      array::const_iterator end (a.end ());
-
-//      CPPUNIT_ASSERT_MESSAGE ("array::begin ()", begin == a.begin ());
-//      CPPUNIT_ASSERT_MESSAGE ("array::end ()", end == a.end ());
-    }
-
-    void
-    test_debug ()
-    {
-      array a = L"[\
-           { \"op\": \"test\", \"path\": \"/a/b/c\", \"value\": \"foo\" },\
-           { \"op\": \"remove\", \"path\": \"/a/b/c\" },\
-           { \"op\": \"add\", \"path\": \"/a/b/c\", \"value\": [ \"foo\", \"bar\" ] },\
-           { \"op\": \"replace\", \"path\": \"/a/b/c\", \"value\": 42 },\
-           { \"op\": \"move\", \"from\": \"/a/b/c\", \"path\": \"/a/b/d\" },\
-           { \"op\": \"copy\", \"from\": \"/a/b/d\", \"path\": \"/a/b/e\" }\
-           { \"pop\": \"copy\", \"from\": \"/a/b/d\", \"path\": \"/a/b/e\" }\
-         ]";
-
-      std::for_each ( a.begin (),
-                      a.end (),
-                      [] (const value & v)
-        {
-          try
-            {
-              const json::value & op = v[L"op"];
-              const wchar_t *str_op = op.as<const wchar_t *> ();
-              std::wcout << op.type () << ": " << str_op << std::endl;
-
-            } catch (const json_error & e) {
-              std::cerr << e.what () << std::endl;
-            }
-        });
-    }
-
-    /**
-     * 10.
-     *
-     * @brief suite
-     * @return
-     *
-    static CppUnit::Test *
-    suite ()
-    {
-      CppUnit::TestSuite *s = new CppUnit::TestSuite ("json array iterator test");
-      s->addTest (new CppUnit::TestCaller<json_array_iterator_test> ("test_construct_assign_destruct", &json_array_iterator_test::test_construct_assign_destruct));
-      s->addTest (new CppUnit::TestCaller<json_array_iterator_test> ("test_dereference", &json_array_iterator_test::test_dereference));
-      s->addTest (new CppUnit::TestCaller<json_array_iterator_test> ("test_pre_increment", &json_array_iterator_test::test_pre_increment));
-      s->addTest (new CppUnit::TestCaller<json_array_iterator_test> ("test_post_increment", &json_array_iterator_test::test_post_increment));
-      s->addTest (new CppUnit::TestCaller<json_array_iterator_test> ("test_begin_end", &json_array_iterator_test::test_begin_end));
-      //s->addTest (new CppUnit::TestCaller<json_array_iterator_test> ("test_debug", &json_array_iterator_test::test_debug));
-      return s;
-    }*/
-  };
-} // Namespace json
+  } // Namespace json
 } // Namespace format
 #endif // JSON_ARRAY_ITERATOR_TEST
 
